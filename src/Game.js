dd943cda43a6440cbab007364d8991e034e6abad
divyanshu <divyanshu.r46956@gmail.com>
2017-05-27 18:31:12 +0530

    Building a game in react.js

---
+++ src/Game.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class Game extends React.Component{
+
+  render(){
+      return(
+          <div>
+            <h3>Wanna Play Game !</h3>
+          </div>
+        )
+  }
+}
+
+
+export default Game;
